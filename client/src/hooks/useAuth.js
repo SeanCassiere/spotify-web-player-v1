98392c2e8d35b428f1cac928aff54b5fb9498072
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-03-21 02:03:29 +0530

    Added: Client-Server Authentication

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -6,12 +6,37 @@ const useAuth = (code) => {
 	const [refreshToken, setRefreshToken] = useState("");
 	const [expiresIn, setExpiresIn] = useState("");
 
+	// Login Logic
 	useEffect(() => {
 		axios
 			.post("http://localhost:3001/login", { code })
-			.then((res) => console.log(res.data))
+			.then((res) => {
+				setAccessToken(res.data.accessToken);
+				setRefreshToken(res.data.refreshToken);
+				setExpiresIn(res.data.expiresIn);
+				window.history.pushState({}, null, "/");
+			})
 			.catch(() => (window.location = "/"));
 	}, [code]);
+
+	// Refresh Token Logic
+	useEffect(() => {
+		if (!refreshToken || !expiresIn) return;
+
+		const interval = setInterval(() => {
+			axios
+				.post("http://localhost:3001/refresh", { refreshToken })
+				.then((res) => {
+					setAccessToken(res.data.accessToken);
+					setExpiresIn(res.data.expiresIn);
+				})
+				.catch(() => (window.location = "/"));
+		}, (expiresIn - 60) * 1000);
+
+		return () => clearInterval(interval);
+	}, [refreshToken, expiresIn]);
+
+	return accessToken;
 };
 
 export default useAuth;
--- server/src/index.js
+++ server/src/index.js
@@ -11,6 +11,29 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.post("/refresh", (req, res) => {
+	const refreshToken = req.body.refreshToken;
+	console.log("refreshing token");
+	const spotifyapi = new SpotifyWebApi({
+		redirectUri: "http://localhost:3000",
+		clientId: process.env.SPOTIFY_CLIENT_ID,
+		clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+		refreshToken,
+	});
+
+	spotifyapi
+		.refreshAccessToken()
+		.then((data) => {
+			res.json({
+				accessToken: data.body.accessToken,
+				expiresIn: data.body.expiresIn,
+			});
+		})
+		.catch(() => {
+			res.sendStatus(400);
+		});
+});
+
 app.post("/login", (req, res) => {
 	const code = req.body.code;
 	const spotifyapi = new SpotifyWebApi({
@@ -28,8 +51,7 @@ app.post("/login", (req, res) => {
 				expiresIn: data.body.expires_in,
 			});
 		})
-		.catch((err) => {
-			console.log(err);
+		.catch(() => {
 			res.sendStatus(400);
 		});
 });
