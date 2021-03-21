f0965ece36efa63373496c3106fd98c643a5b3e7
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-03-22 03:03:23 +0530

    Restored TrackResult onClick to full component

--- client/src/components/TrackSearchResult.jsx
+++ client/src/components/TrackSearchResult.jsx
@@ -8,20 +8,16 @@ const TrackSearchResult = ({ track, chooseTrack }) => {
 	};
 
 	return (
-		<Media className='m-2'>
+		<Media className='m-2' style={cursorStyle} onClick={handlePlay}>
 			<img
 				src={track.albumUrl}
 				width={64}
 				height={64}
 				className='mr-3'
 				alt={track.title}
-				onClick={handlePlay}
-				style={cursorStyle}
 			/>
 			<Media.Body>
-				<h6 onClick={handlePlay} className='p2' style={cursorStyle}>
-					{track.title}
-				</h6>
+				<h6 className='p2'>{track.title}</h6>
 				<div>
 					{track.artistNames.map((artist, i) => (
 						<Badge
